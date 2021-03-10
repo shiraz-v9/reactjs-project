ef691444272244ad634391024ac4165603185c27
kashifAli96 <kashiftauseef@gmail.com>
2021-03-10 03:39:20 +0000

    side bar and editorial section added, eslint config for Hooks get data works with a button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import "./App.css";
 import useWindowDimensions from "./components/windowDimensions";
 import Navbar from "./components/navbar";
-import GetData from "./components/getData";
-import { useState } from "react";
+// import getData from "./components/getData";
+import { useState, useEffect, Component } from "react";
+import Axios from "axios";
+
+function MyComponent() {
+  const colors = {
+    Sea: "#a2ccb6",
+    Sand: "#fceeb5",
+    Peach: "#ee786e",
+  };
+  const [color, setColor] = useState(colors.Sand);
+  useEffect(() => {
+    document.getElementById("sideBar").style.background = color;
+    document.getElementById("sideBar").style.height = "658px";
+  });
+
+  return null;
+}
 
 function App() {
+  const [Data, setData] = useState("");
   const { height, width } = useWindowDimensions();
+  const GetData = () => {
+    Axios.get("http://localhost:5000/home").then((response) => {
+      console.log(response);
+
+      setData(JSON.stringify(response));
+    });
+  };
+
   return (
     <div className="App">
       <Navbar></Navbar>
-      <aside
-        id="aside"
-        style={{ height: 927 - 56, backgroundColor: "#e5e5e5" }}
+      <div
+        id="sideBar"
+        // style={{ height: { height }, backgroundColor: "#e5e5e5" }}
       >
-        Hello world
         <p>
           width: {width} ~ height: {height}
-          data: {GetData}
+          {Data}
         </p>
-      </aside>
+        <button onClick={GetData}>Show data</button>
+      </div>
+      <div id="section">
+        Editorial
+        <MyComponent />
+      </div>
     </div>
   );
 }
--- src/components/getData.js
+++ src/components/getData.js
@@ -1,11 +1,30 @@
-import { useState } from "react";
-// const [employeeList, setEmployeeList] = useState([]);
+import { useState, useEffect } from "react";
+import Axios from "axios";
 
 const GetData = () => {
-  const [employeeList, setEmployeeList] = useState([]);
-  return fetch("http://localhost:5000/home").then((response) => {
-    setEmployeeList(response.setEmployeeList);
+  const [Data, setData] = useState("hello");
+  Axios.get("http://localhost:5000/home").then((response) => {
+    // setData(response);
+    console.log(Data);
+
+    return console.log(response.data);
   });
 };
 
 export default GetData;
+// export default function Example() {
+//   const [count, setCount] = useState(0);
+
+//   // Similar to componentDidMount and componentDidUpdate:
+//   useEffect(() => {
+//     // Update the document title using the browser API
+//     document.title = `You clicked ${count} times`;
+//   });
+
+//   return (
+//     <div>
+//       <p>You clicked {count} times</p>
+//       <button onClick={() => setCount(count + 1)}>Click me</button>
+//     </div>
+//   );
+// }
