531fc31208648b10a92de9e4fe5d8b19aeee7969
kashifAli96 <kashiftauseef@gmail.com>
2021-04-09 22:08:36 +0100

    New text editor highlighter and code preview with <iframe>. Editor needs fixing..

--- src/components/getRequest.js
+++ src/components/getRequest.js
@@ -1,8 +1,18 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import $ from "jquery";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css";
+import { Controlled as CodeMirror } from "react-codemirror2";
 
 function GetRequest() {
+  const [html, setHtml] = useState("");
+  const [css, setCss] = useState("");
+  const [js, setJs] = useState("");
+  const [srcDoc, setSrcDoc] = useState("");
   //Get text from my links and return data from db
   $(document).on("click", "a", function (event) {
     var target = $(event.target).closest("a");
@@ -10,16 +20,62 @@ function GetRequest() {
     setUrl(target.text());
   });
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `);
+    }, 250);
+
+    return () => clearTimeout(timeout);
+  }, [html, css, js]);
+
+  // const handleChange = (editor, data, value) => {
+  //   onChange(setHtml);
+  // };
+
   function ListItem(props) {
+    setHtml(props.example);
     return (
       <div id="dataContainer">
         <h1>HTML tutorial</h1>
         <br></br>
 
-        <h2>{props.tag}</h2>
+        <h2>{props.tag} element</h2>
+        {/* <br></br> */}
+        {/* <code>{props.example}</code> */}
+        <div className="codeMirror">
+          <CodeMirror
+            language="xml"
+            displayName="HTML"
+            // onBeforeChange={handleChange}
+            value={props.example}
+            // onChange={ }
+            className="code-mirror-wrapper"
+            options={{
+              lineWrapping: true,
+              lint: true,
+              mode: "xml",
+              theme: "material",
+              lineNumbers: false,
+            }}
+          />
+        </div>
         <br></br>
-        <code>{props.example}</code>
-        <br></br>
+        <div className="iframe">
+          <iframe
+            srcDoc={srcDoc}
+            title="output"
+            sandbox="allow-scripts"
+            // frameBorder="0"
+            width="50%"
+            // height="100%"
+          />
+        </div>
         <br></br>
         <p>{props.description}</p>
       </div>
